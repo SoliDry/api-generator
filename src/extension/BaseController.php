acdaeae2523a9fb895b2a1ffaeb42cdb410d343b
zeusakm <zeusakm@gmail.com>
2016-12-14 22:14:02 +0300

    *	#12. laravel+raml+json-api impl

---
+++ src/extension/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 14.12.16
+ * Time: 22:12
+ */
+
+namespace rjapi\extension;
+
+
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+    
+}
---
+++ src/extension/BaseModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 14.12.16
+ * Time: 22:05
+ */
+
+namespace rjapi\extension;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    
+}
