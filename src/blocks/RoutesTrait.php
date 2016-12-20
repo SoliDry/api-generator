2f2b4aaf0fd723694254b5c4f2fdb0e15c4f7c93
Arthur <zeusakm@gmail.com>
2016-12-20 17:59:37 +0300

    #17. routes trait

--- src/blocks/RoutesTrait.php
+++ src/blocks/RoutesTrait.php
@@ -10,17 +10,45 @@ namespace rjapi\blocks;
 
 trait RoutesTrait
 {
+    public function openGroup(string $version)
+    {
+        $this->sourceCode .= RoutesInterface::CLASS_ROUTE . PhpEntitiesInterface::DOUBLE_COLON
+                             . RoutesInterface::METHOD_GROUP . PhpEntitiesInterface::OPEN_PARENTHESES
+                             . PhpEntitiesInterface::OPEN_BRACKET
+                             . PhpEntitiesInterface::QUOTES . DefaultInterface::PREFIX_KEY . PhpEntitiesInterface::SPACE
+                             . $version . PhpEntitiesInterface::COMMA . PhpEntitiesInterface::PHP_NAMESPACE
+                             . PhpEntitiesInterface::DOUBLE_ARROW . DirsInterface::MODULES_DIR .
+                             PhpEntitiesInterface::BACKSLASH
+                             . $this->generator->version . DirsInterface::HTTP_DIR
+                             . PhpEntitiesInterface::BACKSLASH . DirsInterface::CONTROLLERS_DIR .
+                             PhpEntitiesInterface::QUOTES
+                             . PhpEntitiesInterface::CLOSE_BRACKET . PhpEntitiesInterface::COMMA
+                             . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::PHP_FUNCTION .
+                             PhpEntitiesInterface::OPEN_PARENTHESES
+                             . PhpEntitiesInterface::CLOSE_PARENTHESES . PHP_EOL;
+
+        $this->sourceCode .= PhpEntitiesInterface::OPEN_BRACE . PHP_EOL;
+    }
+
+    public function closeGroup()
+    {
+        $this->sourceCode .= PhpEntitiesInterface::CLOSE_BRACE . PhpEntitiesInterface::CLOSE_PARENTHESES
+                             . PhpEntitiesInterface::SEMICOLON . PHP_EOL;
+    }
+
     public function setRoute($method, $objectName, $uri, $withId = false)
     {
         $this->sourceCode .= RoutesInterface::CLASS_ROUTE . PhpEntitiesInterface::DOUBLE_COLON
                              . $method . PhpEntitiesInterface::OPEN_PARENTHESES;
 
-        $this->sourceCode .= PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::SLASH . strtolower($objectName)
+        $this->sourceCode .= PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::SLASH
+                             . strtolower($objectName) . (($withId === true) ?
+                PhpEntitiesInterface::SLASH . RamlInterface::RAML_ID : '')
                              . PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::COMMA . PhpEntitiesInterface::SPACE
                              . PhpEntitiesInterface::QUOTES .
-                             $this->generator->objectName . DefaultInterface::CONTROLLER_POSTFIX
-                             . PhpEntitiesInterface::AT . HTTPMethodsInterface::URI_METHOD_INDEX
+                             $objectName . DefaultInterface::CONTROLLER_POSTFIX
+                             . PhpEntitiesInterface::AT . $uri
                              . PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::CLOSE_PARENTHESES .
-                             PhpEntitiesInterface::SEMICOLON;
+                             PhpEntitiesInterface::SEMICOLON . PHP_EOL;
     }
 }
