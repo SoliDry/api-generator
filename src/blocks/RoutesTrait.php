860e54e83015637a3dc11439289de50bc561c105
zeusakm <zeusakm@gmail.com>
2016-12-21 08:47:13 +0300

    *	hot-fix. routes namespace + json api model

--- src/blocks/RoutesTrait.php
+++ src/blocks/RoutesTrait.php
@@ -7,17 +7,7 @@
  */
 
 namespace rjapi\blocks;
-/*
-    This trait creates template similar to:
-    Route::group(['middleware' => 'web', 'prefix' => 'v2', 'namespace' => 'Modules\V2\Http\Controllers'], function()
-    {
-        Route::get('/article', 'ArticleController@index');
-        Route::get('/article/{id}', 'ArticleController@view');
-        Route::post('/article', 'ArticleController@create');
-        Route::patch('/article/{id}', 'ArticleController@update');
-        Route::delete('/article/{id}', 'ArticleController@delete');
-    });
-*/
+
 use rjapi\helpers\Config;
 
 trait RoutesTrait
@@ -25,21 +15,22 @@ trait RoutesTrait
     public function openGroup(string $version)
     {
         $this->sourceCode .= RoutesInterface::CLASS_ROUTE . PhpEntitiesInterface::DOUBLE_COLON
-                             . RoutesInterface::METHOD_GROUP . PhpEntitiesInterface::OPEN_PARENTHESES
-                             . PhpEntitiesInterface::OPEN_BRACKET
-                             . PhpEntitiesInterface::QUOTES . DefaultInterface::PREFIX_KEY . PhpEntitiesInterface::QUOTES
-                             . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::DOUBLE_ARROW . PhpEntitiesInterface::SPACE
-                             . PhpEntitiesInterface::QUOTES . $version . PhpEntitiesInterface::QUOTES
-                             . PhpEntitiesInterface::COMMA . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::QUOTES
-                             . PhpEntitiesInterface::PHP_NAMESPACE . PhpEntitiesInterface::QUOTES
-                             . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::DOUBLE_ARROW
-                             . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::QUOTES. DirsInterface::MODULES_DIR .
-                             PhpEntitiesInterface::BACKSLASH . Config::getModuleName() . DirsInterface::HTTP_DIR
-                             . PhpEntitiesInterface::BACKSLASH . DirsInterface::CONTROLLERS_DIR . PhpEntitiesInterface::QUOTES
-                             . PhpEntitiesInterface::CLOSE_BRACKET . PhpEntitiesInterface::COMMA
-                             . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::PHP_FUNCTION .
-                             PhpEntitiesInterface::OPEN_PARENTHESES
-                             . PhpEntitiesInterface::CLOSE_PARENTHESES . PHP_EOL;
+            . RoutesInterface::METHOD_GROUP . PhpEntitiesInterface::OPEN_PARENTHESES
+            . PhpEntitiesInterface::OPEN_BRACKET
+            . PhpEntitiesInterface::QUOTES . DefaultInterface::PREFIX_KEY . PhpEntitiesInterface::QUOTES
+            . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::DOUBLE_ARROW . PhpEntitiesInterface::SPACE
+            . PhpEntitiesInterface::QUOTES . $version . PhpEntitiesInterface::QUOTES
+            . PhpEntitiesInterface::COMMA . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::QUOTES
+            . PhpEntitiesInterface::PHP_NAMESPACE . PhpEntitiesInterface::QUOTES
+            . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::DOUBLE_ARROW
+            . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::QUOTES. DirsInterface::MODULES_DIR .
+            PhpEntitiesInterface::BACKSLASH . PhpEntitiesInterface::BACKSLASH . Config::getModuleName()
+            . PhpEntitiesInterface::BACKSLASH . PhpEntitiesInterface::BACKSLASH . DirsInterface::HTTP_DIR
+            . PhpEntitiesInterface::BACKSLASH . PhpEntitiesInterface::BACKSLASH . DirsInterface::CONTROLLERS_DIR . PhpEntitiesInterface::QUOTES
+            . PhpEntitiesInterface::CLOSE_BRACKET . PhpEntitiesInterface::COMMA
+            . PhpEntitiesInterface::SPACE . PhpEntitiesInterface::PHP_FUNCTION .
+            PhpEntitiesInterface::OPEN_PARENTHESES
+            . PhpEntitiesInterface::CLOSE_PARENTHESES . PHP_EOL;
 
         $this->sourceCode .= PhpEntitiesInterface::OPEN_BRACE . PHP_EOL;
     }
@@ -47,22 +38,23 @@ trait RoutesTrait
     public function closeGroup()
     {
         $this->sourceCode .= PhpEntitiesInterface::CLOSE_BRACE . PhpEntitiesInterface::CLOSE_PARENTHESES
-                             . PhpEntitiesInterface::SEMICOLON . PHP_EOL;
+            . PhpEntitiesInterface::SEMICOLON . PHP_EOL;
     }
 
     public function setRoute($method, $objectName, $uri, $withId = false)
     {
         $this->sourceCode .= PhpEntitiesInterface::TAB_PSR4 . RoutesInterface::CLASS_ROUTE . PhpEntitiesInterface::DOUBLE_COLON
-                             . $method . PhpEntitiesInterface::OPEN_PARENTHESES;
+            . $method . PhpEntitiesInterface::OPEN_PARENTHESES;
 
         $this->sourceCode .= PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::SLASH
-                             . strtolower($objectName) . (($withId === true) ?
-                PhpEntitiesInterface::SLASH . RamlInterface::RAML_ID : '')
-                             . PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::COMMA . PhpEntitiesInterface::SPACE
-                             . PhpEntitiesInterface::QUOTES .
-                             $objectName . DefaultInterface::CONTROLLER_POSTFIX
-                             . PhpEntitiesInterface::AT . $uri
-                             . PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::CLOSE_PARENTHESES .
-                             PhpEntitiesInterface::SEMICOLON . PHP_EOL;
+            . strtolower($objectName) . (($withId === true) ?
+                PhpEntitiesInterface::SLASH . PhpEntitiesInterface::OPEN_BRACE
+                . RamlInterface::RAML_ID . PhpEntitiesInterface::CLOSE_BRACE : '')
+            . PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::COMMA . PhpEntitiesInterface::SPACE
+            . PhpEntitiesInterface::QUOTES .
+            $objectName . DefaultInterface::CONTROLLER_POSTFIX
+            . PhpEntitiesInterface::AT . $uri
+            . PhpEntitiesInterface::QUOTES . PhpEntitiesInterface::CLOSE_PARENTHESES .
+            PhpEntitiesInterface::SEMICOLON . PHP_EOL;
     }
 }
