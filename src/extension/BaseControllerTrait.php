132b8bb287cdf6a1972dd53855bfdc3017f76359
Arthur <zeusakm@gmail.com>
2016-12-30 11:38:57 +0300

    #22. rel straight create/update + mass assignment fix

--- src/extension/BaseControllerTrait.php
+++ src/extension/BaseControllerTrait.php
@@ -13,11 +13,11 @@ use rjapi\helpers\Json;
 
 trait BaseControllerTrait
 {
-    private $props = [];
-    private $entity = null;
-    private $model = null;
+    private $props       = [];
+    private $entity      = null;
+    private $model       = null;
     private $modelEntity = null;
-    private $middleWare = null;
+    private $middleWare  = null;
 
     private $methods = [
         self::URI_METHOD_INDEX  => self::HTTP_METHOD_GET,
@@ -29,19 +29,20 @@ trait BaseControllerTrait
 
     public function __construct()
     {
-        $this->entity = Classes::cutEntity(Classes::getObjectName($this), DefaultInterface::CONTROLLER_POSTFIX);
+        $this->entity     = Classes::cutEntity(Classes::getObjectName($this), DefaultInterface::CONTROLLER_POSTFIX);
         $middlewareEntity = DirsInterface::MODULES_DIR . PhpEntitiesInterface::BACKSLASH . Config::getModuleName() .
-            PhpEntitiesInterface::BACKSLASH . DirsInterface::HTTP_DIR .
-            PhpEntitiesInterface::BACKSLASH .
-            DirsInterface::MIDDLEWARE_DIR . PhpEntitiesInterface::BACKSLASH .
-            $this->entity .
-            DefaultInterface::MIDDLEWARE_POSTFIX;
+                            PhpEntitiesInterface::BACKSLASH . DirsInterface::HTTP_DIR .
+                            PhpEntitiesInterface::BACKSLASH .
+                            DirsInterface::MIDDLEWARE_DIR . PhpEntitiesInterface::BACKSLASH .
+                            $this->entity .
+                            DefaultInterface::MIDDLEWARE_POSTFIX;
         $this->middleWare = new $middlewareEntity();
-        $this->props = get_object_vars($this->middleWare);
+        $this->props      = get_object_vars($this->middleWare);
 
         $this->modelEntity = DirsInterface::MODULES_DIR . PhpEntitiesInterface::BACKSLASH . Config::getModuleName() .
-            PhpEntitiesInterface::BACKSLASH . DirsInterface::ENTITIES_DIR . PhpEntitiesInterface::BACKSLASH . $this->entity;
-        $this->model = new $this->modelEntity();
+                             PhpEntitiesInterface::BACKSLASH . DirsInterface::ENTITIES_DIR .
+                             PhpEntitiesInterface::BACKSLASH . $this->entity;
+        $this->model       = new $this->modelEntity();
     }
 
     /**
@@ -49,7 +50,7 @@ trait BaseControllerTrait
      */
     public function index()
     {
-        $items = $this->getAllEntities();
+        $items    = $this->getAllEntities();
         $resource = Json::getResource($this->middleWare, $items, $this->entity, true);
         Json::outputSerializedData($resource);
     }
@@ -61,7 +62,7 @@ trait BaseControllerTrait
      */
     public function view(int $id)
     {
-        $item = $this->getEntity($id);
+        $item     = $this->getEntity($id);
         $resource = Json::getResource($this->middleWare, $item, $this->entity);
         Json::outputSerializedData($resource);
     }
@@ -73,22 +74,27 @@ trait BaseControllerTrait
      */
     public function create(Request $request)
     {
-        $json = Json::parse($request->getContent());
+        $json              = Json::parse($request->getContent());
         $jsonApiAttributes = Json::getAttributes($json);
-        foreach ($this->props as $k => $v) {
+        foreach($this->props as $k => $v)
+        {
             // request fields should match Middleware fields
-            if (isset($jsonApiAttributes[$k])) {
+            if(isset($jsonApiAttributes[$k]))
+            {
                 $this->model->$k = $jsonApiAttributes[$k];
             }
         }
         $this->model->save();
 
         $jsonApiRels = Json::getRelationships($json);
-        if (empty($jsonApiRels) === false) {
+        if(empty($jsonApiRels) === false)
+        {
             $lowEntity = strtolower($this->entity);
 
-            foreach ($jsonApiRels as $entity => $value) {
-                foreach ($value[RamlInterface::RAML_DATA] as $index => $val) {
+            foreach($jsonApiRels as $entity => $value)
+            {
+                foreach($value[RamlInterface::RAML_DATA] as $index => $val)
+                {
                     $rId = $val[RamlInterface::RAML_ID];
                     // if pivot file exists then save
                     $ucEntity = ucfirst($entity);
@@ -118,46 +124,54 @@ trait BaseControllerTrait
      * PATCH Updates one entry determined by unique id as uri param for specified fields in $request
      *
      * @param Request $request
-     * @param int $id
+     * @param int     $id
      */
     public function update(Request $request, int $id)
     {
         // get json raw input and parse attrs
-        $json = Json::parse($request->getContent());
+        $json              = Json::parse($request->getContent());
         $jsonApiAttributes = Json::getAttributes($json);
-        $model = $this->getEntity($id);
-        foreach ($this->props as $k => $v) {
+        $model             = $this->getEntity($id);
+        foreach($this->props as $k => $v)
+        {
             // request fields should match Middleware fields
-            if (empty($jsonApiAttributes[$k]) === false) {
+            if(empty($jsonApiAttributes[$k]) === false)
+            {
                 $model->$k = $jsonApiAttributes[$k];
             }
         }
         $model->save();
 
         $jsonApiRels = Json::getRelationships($json);
-        if (empty($jsonApiRels) === false) {
+        if(empty($jsonApiRels) === false)
+        {
             $lowEntity = strtolower($this->entity);
-            foreach ($jsonApiRels as $entity => $value) {
-                foreach ($value[RamlInterface::RAML_DATA] as $index => $val) {
+            foreach($jsonApiRels as $entity => $value)
+            {
+                foreach($value[RamlInterface::RAML_DATA] as $index => $val)
+                {
                     $rId = $val[RamlInterface::RAML_ID];
                     // if pivot file exists then save
                     $ucEntity = ucfirst($entity);
-                    $file = DirsInterface::MODULES_DIR . PhpEntitiesInterface::SLASH
-                            . Config::getModuleName() . PhpEntitiesInterface::SLASH .
-                            DirsInterface::ENTITIES_DIR .
-                            $this->entity . $ucEntity . PhpEntitiesInterface::PHP_EXT;
-                    if (file_exists($file)) { // ManyToMany rel
+                    $file     = DirsInterface::MODULES_DIR . PhpEntitiesInterface::SLASH
+                                . Config::getModuleName() . PhpEntitiesInterface::SLASH .
+                                DirsInterface::ENTITIES_DIR .
+                                $this->entity . $ucEntity . PhpEntitiesInterface::PHP_EXT;
+                    if(file_exists($file))
+                    { // ManyToMany rel
                         // clean up old links
-                         $this->getModelEntities($this->entity . $ucEntity,
-                             [$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID, $rId])->delete();
+                        $this->getModelEntities(
+                            $this->entity . $ucEntity,
+                            [$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID, $rId]
+                        )->delete();
                         // set up new links
                         $pivot = new $this->entity . $ucEntity();
                         $pivot->{$entity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $rId;
                         $pivot->{$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $this->model->id;
                     } else { // OneToOne
                         $refModel = new $ucEntity();
-                        $model = $this->getModelEntity($refModel, $rId);
-                        $model->{$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $this->model->id;
+                        $model = $this->getModelEntities($refModel, [$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID, $rId]);
+                        $model->update([$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID => $this->model->id]);
                         $model->save();
                     }
                 }
@@ -176,7 +190,8 @@ trait BaseControllerTrait
     public function delete(int $id)
     {
         $model = $this->getEntity($id);
-        if ($model !== null) {
+        if($model !== null)
+        {
             $model->delete();
             $resource = Json::getResource($this->middleWare, $model, $this->entity);
             Json::outputSerializedData($resource, JSONApiInterface::HTTP_RESPONSE_CODE_NO_CONTENT);
@@ -185,14 +200,112 @@ trait BaseControllerTrait
 
     /**
      * GET the relationships of this particular Entity
-     * @param int $id
+     *
+     * @param int    $id
      * @param string $relation
      */
     public function relations(int $id, string $relation)
     {
-        $item = $this->getEntity($id);
+        $item     = $this->getEntity($id);
         $resource = Json::getResource($this->middleWare, $item->$relation, $this->entity);
         Json::outputSerializedData($resource);
+    }
+
+    /**
+     * POST relationships for specific entity id
+     *
+     * @param Request $request
+     * @param int     $id
+     * @param string  $relation
+     */
+    public function createRelations(Request $request, int $id, string $relation)
+    {
+        $json        = Json::parse($request->getContent());
+        $jsonApiRels = Json::getRelationships($json);
+        if(empty($jsonApiRels) === false)
+        {
+            foreach($jsonApiRels as $entity => $value)
+            {
+                foreach($value[RamlInterface::RAML_DATA] as $index => $val)
+                {
+                    $rId       = $val[RamlInterface::RAML_ID];
+                    $ucEntity  = ucfirst($entity);
+                    $lowEntity = strtolower($this->entity);
+                    $file      = DirsInterface::MODULES_DIR . PhpEntitiesInterface::SLASH
+                                 . Config::getModuleName() . PhpEntitiesInterface::SLASH .
+                                 DirsInterface::ENTITIES_DIR .
+                                 $this->entity . $ucEntity . PhpEntitiesInterface::PHP_EXT;
+                    if(file_exists($file))
+                    { // ManyToMany rel
+                        $pivot = new $this->entity . $ucEntity();
+                        $pivot->{$entity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $rId;
+                        $pivot->{$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $this->model->id;
+                    }
+                    else
+                    { // ToOne
+                        $refModel = new $ucEntity();
+                        $model = $this->getModelEntity($refModel, $rId);
+                        $model->{$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $this->model->id;
+                        $model->save();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * PATCH relationships for specific entity id
+     *
+     * @param int    $id
+     * @param string $relation
+     */
+    public function updateRelations(int $id, string $relation)
+    {
+        $jsonApiRels = Json::getRelationships($json);
+        if(empty($jsonApiRels) === false)
+        {
+            $lowEntity = strtolower($this->entity);
+            foreach($jsonApiRels as $entity => $value)
+            {
+                foreach($value[RamlInterface::RAML_DATA] as $index => $val)
+                {
+                    $rId = $val[RamlInterface::RAML_ID];
+                    // if pivot file exists then save
+                    $ucEntity = ucfirst($entity);
+                    $file     = DirsInterface::MODULES_DIR . PhpEntitiesInterface::SLASH
+                                . Config::getModuleName() . PhpEntitiesInterface::SLASH .
+                                DirsInterface::ENTITIES_DIR .
+                                $this->entity . $ucEntity . PhpEntitiesInterface::PHP_EXT;
+                    if(file_exists($file))
+                    { // ManyToMany rel
+                        // clean up old links
+                        $this->getModelEntities(
+                            $this->entity . $ucEntity,
+                            [$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID, $rId]
+                        )->delete();
+                        // set up new links
+                        $pivot = new $this->entity . $ucEntity();
+                        $pivot->{$entity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $rId;
+                        $pivot->{$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID} = $this->model->id;
+                    } else { // OneToOne
+                        $refModel = new $ucEntity();
+                        $model = $this->getModelEntities($refModel, [$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID, $rId]);
+                        $model->update([$lowEntity . PhpEntitiesInterface::UNDERSCORE . RamlInterface::RAML_ID => $this->model->id]);
+                        $model->save();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * DELETE relationships for specific entity id
+     *
+     * @param int    $id
+     * @param string $relation
+     */
+    public function deleteRelations(int $id, string $relation)
+    {
     }
 
     /**
@@ -249,9 +362,10 @@ trait BaseControllerTrait
     private function getAllEntities(int $count = ModelsInterface::DEFAULT_LIMIT, int $page = 1)
     {
         $from = ($count * $page) - $count;
-        $to = $count * $page;
-        $obj = call_user_func_array(
-            PhpEntitiesInterface::BACKSLASH . $this->modelEntity . PhpEntitiesInterface::DOUBLE_COLON . ModelsInterface::MODEL_METHOD_ORDER_BY,
+        $to   = $count * $page;
+        $obj  = call_user_func_array(
+            PhpEntitiesInterface::BACKSLASH . $this->modelEntity . PhpEntitiesInterface::DOUBLE_COLON .
+            ModelsInterface::MODEL_METHOD_ORDER_BY,
             [RamlInterface::RAML_ID, ModelsInterface::SQL_DESC]
         );
 
