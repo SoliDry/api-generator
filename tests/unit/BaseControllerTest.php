2c7d1fb2b394f146f075f87b0d5abf1b76e72956
zeusakm <zeusakm@gmail.com>
2017-01-15 23:10:54 +0300

    *	#issue-43. phpunit coverage test

---
+++ tests/unit/BaseControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Routing\Route;
+use PHPUnit\Framework\TestCase;
+use rjapi\extension\BaseController;
+
+class BaseControllerTest extends TestCase
+{
+    public function testConstruction()
+    {
+        $route = new Route(['foo'], '/v1/bar', ['index']);
+        $obj = new BaseController($route);
+    }
+}
