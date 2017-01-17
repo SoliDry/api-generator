c32fb2b8e1057c8ba92a836c98bb2a38d3c02dfa
Arthur <zeusakm@gmail.com>
2017-01-17 15:14:49 +0300

    *	#issue-43. add

--- tests/unit/TestCase.php
+++ tests/unit/TestCase.php
@@ -16,6 +16,7 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
+        config(['app.timezone' => 'Europe/Moscow']);
         $app = require __DIR__ . '/../../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -2,6 +2,7 @@
 // Here you can initialize variables that will be available to your tests
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../app/Console/Kernel.php';
+
 spl_autoload_register(
     function($class)
     {
