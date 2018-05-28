d1672786444357bc73308019319159ea008ea8eb
zeusakm <zeusakm@gmail.com>
2018-05-28 09:15:56 +0300

    #111. Add more coverage on Controllers

--- tests/unit/blocks/ControllersTest.php
+++ tests/unit/blocks/ControllersTest.php
@@ -2,9 +2,13 @@
 
 namespace rjapitest\unit\blocks;
 
+use Illuminate\Routing\Route;
+use Modules\V2\Http\Controllers\ArticleController;
 use rjapi\blocks\Controllers;
+use rjapi\extension\ApiController;
 use rjapi\RJApiGenerator;
 use rjapi\types\ControllersInterface;
+use rjapi\types\DirsInterface;
 use rjapitest\unit\TestCase;
 
 class ControllersTest extends TestCase
@@ -15,7 +19,61 @@ class ControllersTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->controller = new Controllers(new RJApiGenerator());
+        $gen                 = new RJApiGenerator();
+        $gen->objectName     = 'Article';
+        $gen->version        = 'v2';
+        $gen->modulesDir     = DirsInterface::MODULES_DIR;
+        $gen->controllersDir = DirsInterface::CONTROLLERS_DIR;
+        $gen->httpDir        = DirsInterface::HTTP_DIR;
+        $gen->types          = [
+            'SID'               => [
+                'type'      => 'string',
+                'required'  => true,
+                'maxLength' => 128,
+            ],
+            'ArticleAttributes' => [
+                'description' => 'Article attributes description',
+                'type'        => 'object',
+                'properties'  => [
+                    'title' => [
+                        'required'  => true,
+                        'type'      => 'string',
+                        'minLength' => 16,
+                        'maxLength' => 256,
+                        'facets'    => [
+                            'index' => [
+                                'idx_title' => 'index'
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'Article'           => [
+                'type'       => 'object',
+                'properties' => [
+                    'type'          => 'Type',
+                    'id'            => 'SID',
+                    'attributes'    => 'ArticleAttributes',
+                    'relationships' => [
+                        'type' => 'TagRelationships[] | TopicRelationships',
+                    ]
+                ]
+            ]
+        ];
+        $this->controller    = new Controllers($gen);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_entity()
+    {
+        $this->controller->createEntity('./tests/_output', 'ControllerTest');
+        $this->assertTrue(file_exists(self::DIR_OUTPUT . 'ArticleControllerTest.php'));
+        require_once __DIR__ . '/../../_output/ArticleControllerTest.php';
+        $articleController = new ArticleController(new Route(['POST', 'GET'], '', function () {
+        }));
+        $this->assertInstanceOf(ApiController::class, $articleController);
     }
 
     /**
