1a569ad04f78d952f46e526a1b51b4d064008d56
arthurkushman <zeusakm@gmail.com>
2019-01-29 10:11:12 +0300

    #134. Fix NO_CONTENT crash with ResponseFactory on empty data coming from Laravel core

--- src/extension/ApiController.php
+++ src/extension/ApiController.php
@@ -316,7 +316,9 @@ class ApiController extends Controller implements JSONApiInterface
     public function getResponse(string $json, int $responseCode = JSONApiInterface::HTTP_RESPONSE_CODE_OK) : Response
     {
         if ($responseCode === JSONApiInterface::HTTP_RESPONSE_CODE_NO_CONTENT) {
-            return response()->withHeaders(JSONApiInterface::STANDARD_HEADERS)->setStatusCode($responseCode);
+            // ! it is important to pass empty string here, otherwise there will be Exception from ResponseFactory
+            // seems like a bug in Laravel - either don't have time to report them and don't like their PR rules
+            return response('')->withHeaders(JSONApiInterface::STANDARD_HEADERS)->setStatusCode($responseCode);
         }
 
         return response($json)->withHeaders(JSONApiInterface::STANDARD_HEADERS)->setStatusCode($responseCode);
